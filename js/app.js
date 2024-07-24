9303e1cf0a087927808748a3795260cedfdbdd6b
Thaynan-Ferreira <thaynan_ferreira@outlook.com.br>
2024-07-24 12:17:03 -0300

    Implementando a função para cadeira Superior

--- js/app.js
+++ js/app.js
@@ -2,15 +2,26 @@ function comprar(){
     let tipo = document.querySelector('#tipo-ingresso').value;
     let quantidade = document.querySelector('#qtd').value;
     let qtdInferior = document.querySelector('#qtd-inferior')
+    let qtdSuperior = document.querySelector('#qtd-superior')
+    let qtdPista = document.querySelector('#qtd-pista')
 
     if(tipo == 'inferior'){
-        if(quantidade <= qtdInferior.textContent){
-            let qtd = parseInt(qtdInferior.textContent);
+        if(quantidade <= parseInt(qtdInferior.textContent)){
+            let qtd = qtdInferior.textContent;
             qtdInferior.textContent = qtd - quantidade;
         }
         else{
             alert(`quantidade indisponivel para ${tipo}`)
         }
-        
+    }
+    
+    else if(tipo == 'superior'){
+        if(quantidade <= parseInt(qtdSuperior.textContent)){
+            let qtd = qtdSuperior.textContent;
+            qtdSuperior.textContent = qtd - quantidade;
+        }
+        else{
+            alert(`quantidade indisponivel para ${tipo}`)
+        }
     }
 }
